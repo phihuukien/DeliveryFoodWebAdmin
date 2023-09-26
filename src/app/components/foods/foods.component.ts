ff307085358ed51cc51f2f6b2bd42e4a57c12684
dattai2k2 <dattai2k2@gmai.com>
2023-09-26 22:57:19 +0700

    tai commit admin

--- src/app/components/foods/foods.component.ts
+++ src/app/components/foods/foods.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Food } from 'src/app/Models/Food';
+import { FoodService } from 'src/app/services/foods/food.service';
 
 @Component({
   selector: 'app-foods',
   templateUrl: './foods.component.html',
   styleUrls: ['./foods.component.css']
 })
-export class FoodsComponent {
-
+export class FoodsComponent implements OnInit {
+  restaurantId!: string;
+  foods:any=Food;
+  constructor(private cookieService: CookieService, private foodService: FoodService) {
+  }
+  ngOnInit(): void {
+    this.restaurantId = this.cookieService.get('restaurantId');
+    this.getAll();
+  }
+  getAll() {
+    this.foodService.getAllFoods(this.restaurantId).subscribe((response: any) => {
+      if (response.status) {
+        this.foods = response.data;
+        console.log(this.foods);
+      }
+    })
+  }
 }
